70cf0831398b44f00a02e17bbe8814b9dbca35d2
Michael Wittig <michael@widdix.de>
2020-06-25 11:18:19 +0200

    fix id

--- test/defaults.js
+++ test/defaults.js
@@ -4,7 +4,9 @@ const cfntest = require('@cfn-modules/test');
 test.serial('defaults', async t => {
   const stackName = cfntest.stackName();
   try {
-    t.log(await cfntest.createStack(`${__dirname}/defaults.yml`, stackName, {}));
+    t.log(await cfntest.createStack(`${__dirname}/defaults.yml`, stackName, {
+      HostedZoneId: 'Z08057981AMLWUWNFCQLS'
+    }));
     // what could we test here?
   } finally {
     t.log(await cfntest.deleteStack(stackName));
